f46a4dcbfb4597359440105661d387c5d959312e
smaartscz <65630155+smaartscz@users.noreply.github.com>
2024-01-19 19:06:04 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# Concerts Webhook
+ 
---
+++ main.py
@@ -0,0 +1,14 @@
+from discord_webhook import DiscordWebhook
+#Load config
+
+def create_config():
+
+def save_config():
+
+def load_config():
+
+
+try:
+    load_config()
+except:
+   create_config()
